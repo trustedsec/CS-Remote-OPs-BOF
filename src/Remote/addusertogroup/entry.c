14d1a8bca445c100021c69d03f1fda1b2c57182f
Christopher Paschen <christopher.paschen@trustedsec.com>
2023-02-16 08:46:33 -0800

    allow addusertogroup to fallback to local groups

--- src/Remote/addusertogroup/entry.c
+++ src/Remote/addusertogroup/entry.c
@@ -6,17 +6,31 @@
 #include "base.c"
 
 
-DWORD AddUserToGroup(LPCWSTR lpswzServer, LPCWSTR lpswzUserName, LPCWSTR lpswzGroupName)
+DWORD AddUserToGroup(LPCWSTR lpswzServer, LPCWSTR lpswzUserName, LPCWSTR lpswzGroupName, LPCWSTR lpswzDomainName)
 {
 	NET_API_STATUS dwErrorCode = NERR_Success;
+	LOCALGROUP_MEMBERS_INFO_3 mi[1] = {0};
 	dwErrorCode = NETAPI32$NetGroupAddUser(lpswzServer, lpswzGroupName, lpswzUserName);
 	if(NERR_Success != dwErrorCode)
 	{
 		BeaconPrintf(CALLBACK_ERROR, "Unable to add user to group %lX\n", dwErrorCode);
-		return dwErrorCode;
-	} 
+		BeaconPrintf(CALLBACK_OUTPUT, "Trying to add to local group instead");
+		mi[0].lgrmi3_domainandname = intAlloc(1024);
+		if (lpswzDomainName != NULL)
+		{
+			MSVCRT$wcscat(mi[0].lgrmi3_domainandname, lpswzDomainName);
+			MSVCRT$wcscat(mi[0].lgrmi3_domainandname, L"\\");
+		}
+		MSVCRT$wcscat(mi[0].lgrmi3_domainandname, lpswzUserName);
 
-	internal_printf("User should be added to group\n");
+		BeaconPrintf(CALLBACK_OUTPUT, "Trying to add %ls to local group instead", mi[0].lgrmi3_domainandname);	
+		dwErrorCode = NETAPI32$NetLocalGroupAddMembers(lpswzServer, lpswzGroupName, 3, (LPBYTE)mi, 1);
+		if(NERR_Success != dwErrorCode)
+		{
+			BeaconPrintf(CALLBACK_ERROR, "Unable to add user to local group %lX\n", dwErrorCode);
+		}
+		
+	} 
 
 	return dwErrorCode;
 }
@@ -29,9 +43,12 @@ VOID go(
 	DWORD dwErrorCode = ERROR_SUCCESS;
 	datap parser = {0};
 	BeaconDataParse(&parser, Buffer, Length);
+	LPCWSTR lpswzDomainName = (LPCWSTR)BeaconDataExtract(&parser, NULL); // $5
 	LPCWSTR lpswzHostName = (LPCWSTR)BeaconDataExtract(&parser, NULL); // $4
 	LPCWSTR lpswzUserName = (LPCWSTR)BeaconDataExtract(&parser, NULL); // $2
 	LPCWSTR lpswzGroupName = (LPCWSTR)BeaconDataExtract(&parser, NULL);// $3
+	if(lpswzHostName[0] == L'\0'){lpswzHostName = NULL;}
+	if(lpswzDomainName[0] == L'\0'){lpswzDomainName = NULL;}
 
 	if(!bofstart())
 	{
@@ -40,7 +57,7 @@ VOID go(
 
 	internal_printf("Adding %S to %S\n", lpswzUserName, lpswzGroupName);
 
-	dwErrorCode = AddUserToGroup(lpswzHostName, lpswzUserName, lpswzGroupName);
+	dwErrorCode = AddUserToGroup(lpswzHostName, lpswzUserName, lpswzGroupName, lpswzDomainName);
 	if ( ERROR_SUCCESS != dwErrorCode )
 	{
 		BeaconPrintf(CALLBACK_ERROR, "Adding user to group failed: %lX\n", dwErrorCode);
